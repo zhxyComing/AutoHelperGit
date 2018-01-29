ce2f157ccaeaf5d3ef6c21010f263a0a76933d13
dixon.xu <dixon.xu@dji.com>
2018-01-29 12:20:05 +0800

    first commit

---
+++ app/src/main/java/com/app/xz/autohelper/AutoService.java
@@ -0,0 +1,213 @@
+package com.app.xz.autohelper;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+public class AutoService extends AccessibilityService {
+
+    private static final String TAG = "testkkk";
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        int eventType = event.getEventType();
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                Log.e(TAG, "click");
+                Log.e(TAG, event.getSource().getClassName() + "");
+                //界面点击
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                Log.e(TAG, "text changed");
+                //界面文字改动
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                Log.e(TAG, "notification changed");
+                handleNotification(event);
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                Log.e(TAG, "window state/content changed");
+                String className = event.getClassName().toString();
+                if (className.equals("com.tencent.mm.ui.LauncherUI")) {
+                    getPacket();
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
+                    openPacket();
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")) {
+                    close();
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.e(TAG, "onServiceConnected");
+        initServiceInfo();
+    }
+
+    @Override
+    protected boolean onKeyEvent(KeyEvent event) {
+        Log.e(TAG, "onKeyEvent");
+        return super.onKeyEvent(event);
+    }
+
+    //监听指定package,检索窗口内容,获取事件类型的时间等等.
+    private void initServiceInfo() {
+        AccessibilityServiceInfo serviceInfo = new AccessibilityServiceInfo();
+        serviceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;//监听事件类型 这里是所有事件
+        serviceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;//反馈方式 语音还是振动
+        serviceInfo.packageNames = new String[]{"com.tencent.mm", "com.app.xz.onefish"};//监听的包
+        serviceInfo.notificationTimeout = 100;//事件的时间间隔
+//        canRetrieveWindowContent:表示该服务能否访问活动窗口中的内容.也就是如果你希望在服务中获取窗体内容的化,则需要设置其值为true.
+        setServiceInfo(serviceInfo);
+    }
+
+    //检测服务是否开启
+    private boolean checkStealFeature(String service) {
+        int ok = 0;
+        try {
+            ok = Settings.Secure.getInt(getApplicationContext().getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+        }
+
+        TextUtils.SimpleStringSplitter ms = new TextUtils.SimpleStringSplitter(':');
+        if (ok == 1) {
+            String settingValue = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                ms.setString(settingValue);
+                while (ms.hasNext()) {
+                    String accessibilityService = ms.next();
+                    if (accessibilityService.equalsIgnoreCase(service)) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 处理通知栏信息
+     *
+     * 如果是微信红包的提示信息,则模拟点击
+     *
+     * @param event
+     */
+    private void handleNotification(AccessibilityEvent event) {
+        List<CharSequence> texts = event.getText();
+        if (!texts.isEmpty()) {
+            for (CharSequence text : texts) {
+                String content = text.toString();
+                //如果微信红包的提示信息,则模拟点击进入相应的聊天窗口
+                if (content.contains("[微信红包]")) {
+                    if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification) {
+                        Notification notification = (Notification) event.getParcelableData();
+                        PendingIntent pendingIntent = notification.contentIntent;
+                        try {
+                            pendingIntent.send();
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 关闭红包详情界面,实现自动返回聊天窗口
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void close() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null) {
+            //为了演示,直接查看了关闭按钮的id
+            List<AccessibilityNodeInfo> infos = nodeInfo.findAccessibilityNodeInfosByViewId("@id/ez");
+            nodeInfo.recycle();
+            for (AccessibilityNodeInfo item : infos) {
+                item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+    }
+
+    /**
+     * 模拟点击,拆开红包
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void openPacket() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null) {
+            //为了演示,直接查看了红包控件的id
+            List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("@id/b9m");
+            nodeInfo.recycle();
+            for (AccessibilityNodeInfo item : list) {
+                item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+    }
+
+    /**
+     * 模拟点击,打开抢红包界面
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void getPacket() {
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        AccessibilityNodeInfo node = recycle(rootNode);
+
+        node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        AccessibilityNodeInfo parent = node.getParent();
+        while (parent != null) {
+            if (parent.isClickable()) {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                break;
+            }
+            parent = parent.getParent();
+        }
+
+    }
+
+    /**
+     * 递归查找当前聊天窗口中的红包信息
+     *
+     * 聊天窗口中的红包都存在"领取红包"一词,因此可根据该词查找红包
+     *
+     * @param node
+     */
+    public AccessibilityNodeInfo recycle(AccessibilityNodeInfo node) {
+        if (node.getChildCount() == 0) {
+            if (node.getText() != null) {
+                if ("领取红包".equals(node.getText().toString())) {
+                    return node;
+                }
+            }
+        } else {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                if (node.getChild(i) != null) {
+                    recycle(node.getChild(i));
+                }
+            }
+        }
+        return node;
+    }
+
+}
